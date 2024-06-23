abef0ff662ce26cbcebb8e238c7915afc5affb63
aidis <15270739262@163.com>
2024-06-23 21:29:44 +0800

    2024-06-23 - 21.29分，在公司提交，验证家里

--- spring-study/src/main/java/com/mybatisdemo/springstudy/SpringStudyApplication.java
+++ spring-study/src/main/java/com/mybatisdemo/springstudy/SpringStudyApplication.java
@@ -2,12 +2,13 @@ package com.mybatisdemo.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringStudyApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringStudyApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringStudyApplication.class, args);
+        System.out.println("在公司进行的开发，家里可以看到吗？？");
+        System.out.println("你估计需要解决冲突合并的方式");
     }
-
 }
